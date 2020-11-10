9e919b614fa5d26202390ee5ba47b5f41ae200e3
Roman Stolyarov <romkas2000@gmail.com>
2020-11-09 17:13:34 -0800

    Changes to Purpose section --> into columns

--- src/About.js
+++ src/About.js
@@ -5,14 +5,33 @@ import './About.css';
 function About() {
   return (
     <html>
-        <div class="about-section">
-           <h1>About Us Page</h1>
-           <p>Some text about who we are and what we do.</p>
-           <hr/>
+
+        <div class="purpose-section">
+            <h1>What is Voter App?</h1>
         </div>
 
-        <h2>Our Team</h2>
+        <div class="purpose-row">
+            <div class="purpose-column">
+                <h2>Column title</h2>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            </div>
+
+            <div class="purpose-column">
+                <h2>Column title</h2>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            </div>
+
+            <div class="purpose-column">
+                <h2>Column title</h2>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            </div>
+        </div>
+        
         <div class="row">
+            <div class="ourteam-section">
+                <br/>
+                <h2>Our Team</h2>
+            </div>
             <div class="column">
                 <div class="card">
                     <div class="container">
