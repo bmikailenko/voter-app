33fbe9698eccd47c72fc2345db6d46966de164c5
bmikailenko <ben.mikailenko@wsu.edu>
2020-11-23 10:28:50 -0800

    added initial functionality for admins

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,12 +6,14 @@ import { Link } from 'react-router-dom';
 import "survey-react/survey.css";
 import './App.css';
 
-
 function Dashboard() {
   var [userSurvey, setUserSurvey] = useState();  
+  var [userGroup, setUserGroup] = useState(null);
+
   useEffect(() => {
     const getUserSurvey = async () => {
-      const user = await Auth.currentUserInfo();
+      const user = await Auth.currentAuthenticatedUser();
+      const group = await user.signInUserSession.idToken.payload['cognito:groups'];
       const sub = await user.attributes.sub;
       const userSurvey = await fetchSurvey(sub);
       if (userSurvey) {
@@ -19,9 +21,33 @@ function Dashboard() {
       } else {
         console.log("no user survey yet!");
       }
+      if (group.includes('admin')) {
+        setUserGroup('admin');
+      }
     }
     getUserSurvey();
   },[userSurvey]);
+
+  let nextToken;
+
+  async function listUsers(limit){
+    let apiName = 'AdminQueries';
+    let path = '/listUsers';
+    let myInit = { 
+        queryStringParameters: {
+          "limit": limit,
+          "token": nextToken
+        },
+        headers: {
+          'Content-Type' : 'application/json',
+          Authorization: `${(await Auth.currentSession()).getAccessToken().getJwtToken()}`
+        }
+    }
+    const { NextToken, ...rest } =  await API.get(apiName, path, myInit);
+    nextToken = NextToken;
+    console.log(rest);
+    return rest;
+  }
 
   const fetchSurvey = async (sub) => {
     try {
@@ -33,7 +59,7 @@ function Dashboard() {
     }
   }
 
-  return (
+  return (userGroup !== 'admin') ? (
       <div>
         <AmplifySignOut />
         <h1>My Dashboard</h1>
@@ -48,6 +74,12 @@ function Dashboard() {
         <p>{userSurvey}</p>
         </div>
       </div>
+  ) :
+  (
+    <div>
+      Hah you rock
+      <button onClick={() => listUsers(10)}>List users</button>
+    </div>
   );
 }
 
