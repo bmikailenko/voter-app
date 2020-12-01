ccad880f7cc4c76ff130ee18b835f8b428ce8ca1
Daniel Yarmolenko <35641021+danyarmo@users.noreply.github.com>
2020-11-30 17:14:48 -0800

    made name and description functional

--- src/AboutCandidate.js
+++ src/AboutCandidate.js
@@ -7,21 +7,35 @@ import "survey-react/survey.css";
 import './App.css';
 
 
-function AboutCandidate(){
+function AboutCandidate() {
+    var [candidateName, setCandidateName] = useState('');
+    var [candidateDesc, setCandidateDesc] = useState('');
+    const updateName = (e) => {
+        setCandidateName(e.target.value);
+    }
+    const updateDesc = (e) => {
+        setCandidateDesc(e.target.value);
+    }
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log( candidateName);
+        console.log( candidateDesc);
+    }
     return (
         <div>
             <h1>AboutCandidate</h1>
             <div class="inputContainer">
+                <form onSubmit={submitForm}>
+                    <label for="name">Name: </label>
+                    <input type="text" name="name" onChange={updateName} />
+                    <p>
+                        <label for="description"> Description: </label>
+                        <textarea type="text" name="description" onChange={updateDesc}></textarea>
+                    </p>
+                    <button>Submit</button>
+                </form>
                 <label for="pfp">Profile Picture: </label>
                 <input type="file"></input><br></br>
-                <label for="fname">First name: </label>
-                <input type="text" id="fname" name="fname"></input><br></br>
-                <label for="lname">Last name: </label>
-                <input type="text" id="lname" name="lname"></input><br></br>
-                <p>
-                    <label>Description: </label>
-                    <textarea></textarea>
-                </p>
                 <label for="document">Documentation: </label>
                 <input type="file"></input><br></br>
                 <input type="submit" value="submit"></input>
